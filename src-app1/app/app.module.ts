6081640a9d05733a7e4d483fb20c33d689953528
Ankita Thombare <anku1234thombare@gmail.com>
2020-12-12 16:53:02 +0530

    project added

---
+++ src-app1/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { ForthComponent } from './forth/forth.component';
+import { FifthComponent } from './fifth/fifth.component';
+import { SixthComponent } from './sixth/sixth.component';
+import { SeventhComponent } from './seventh/seventh.component';
+import { EighthComponent } from './eighth/eighth.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent,
+    ForthComponent,
+    FifthComponent,
+    SixthComponent,
+    SeventhComponent,
+    EighthComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src-app1/app/seventh/seventh.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-seventh',
+  templateUrl: './seventh.component.html',
+  styleUrls: ['./seventh.component.css']
+})
+export class SeventhComponent implements OnInit {
+
+  products = [
+    { id: 1, title: 'product 1', price: 100, description: 'description 1' },
+    { id: 2, title: 'product 2', price: 200, description: 'description 2' },
+    { id: 3, title: 'product 3', price: 300, description: 'description 3' },
+    { id: 4, title: 'product 4', price: 400, description: 'description 4' },
+    { id: 5, title: 'product 5', price: 500, description: 'description 5' }
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
